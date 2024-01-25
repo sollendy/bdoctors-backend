c1db174c5db3d8594e735fc6154555abfada50e0
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2024-01-25 12:45:21 +0100

    added client side password validation

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,28 @@
 import './bootstrap';
+import * as bootstrap from 'bootstrap';
+import.meta.glob([
+    '../img/**'
+])
+
+const passwordValue = document.getElementById("password-confirm");
+const confirmButton = document.getElementById("confirm-button");
+
+function checkPassword() {
+    const secondPassword = document.getElementById("password-confirm").value;
+    const firstPassword = document.getElementById("password").value;
+
+    if (firstPassword !== secondPassword || firstPassword.length < 8) {
+        document.getElementById("password-error").innerHTML = "Le password devono essere uguali e devono avere una lunghezza minima di 8 caratteri";
+        document.getElementById("password-confirm").classList.add("is-invalid");
+        document.getElementById("submit-button").setAttribute("disabled", true);
+    } else {
+        document.getElementById("password-error").innerHTML = "";
+        passwordValue.classList.remove("is-invalid");
+        confirmButton.removeAttribute("disabled");
+    }
+
+}
+
+if (passwordValue) {
+    passwordValue.addEventListener("keyup", checkPassword);
+}
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,7 +12,7 @@
                         @csrf
 
                         <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Nome') }}</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Nome *') }}</label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
@@ -26,7 +26,7 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="lastname" class="col-md-4 col-form-label text-md-end">{{ __('Cognome') }}</label>
+                            <label for="lastname" class="col-md-4 col-form-label text-md-end">{{ __('Cognome *') }}</label>
 
                             <div class="col-md-6">
                                 <input id="lastname" type="text" class="form-control @error('lastname') is-invalid @enderror" name="lastname" value="{{ old('lastname') }}" required autocomplete="lastname" autofocus>
@@ -40,7 +40,7 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Indirizzo Email') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Indirizzo Email *') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
@@ -54,7 +54,7 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password *') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
@@ -68,20 +68,28 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Conferma Password') }}</label>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Conferma Password *') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                <div id="password-error" class="text-danger py-1 invalid-feedback">
+
+                                </div>
                             </div>
                         </div>
 
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" id="confirm-button" class="btn btn-primary" disabled>
                                     {{ __('Register') }}
                                 </button>
                             </div>
                         </div>
+
+                        <span class="fs-9">
+                            * indica i campi obbligatori
+                        </span>
+
                     </form>
                 </div>
             </div>
