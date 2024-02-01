a79bd94156e4405dbebe7e7208d26ccce041c162
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2024-02-01 16:13:59 +0100

    added seeder of stars

---
+++ app/Http/Controllers/Api/StarController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Star;
+
+class StarController extends Controller
+{
+    public function store(Request $request, $id)
+    {
+        //
+    }
+}
