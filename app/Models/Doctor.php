9a6bcc21b50faf9478b1ee4cc93fa69ee511fefc
francescotrudu <francescotrudu84@gmail.com>
2024-02-01 12:37:20 +0100

    Api index and show doctor

---
+++ app/Models/Doctor.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Doctor extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['address', 'description', 'visible', 'photo', 'services'];
+
+    protected $with = ['typologies'];
+
+    public static function generateSlug($name)
+    {
+        return Str::slug($name, '-');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function typologies()
+    {
+        return $this->belongsToMany(Typology::class);
+    }
+
+    public function stars()
+    {
+        return $this->belongsToMany(Star::class);
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
+
+    public function getPhotoUri()
+    {
+        // dd($this->image);
+        return $this->photo ? asset('storage/' . $this->photo) : "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/681px-Placeholder_view_vector.svg.png";
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/doctors', [DoctorController::class, 'index']);
+Route::get('/doctors', [DoctorController::class, 'searchBySpecialization']);
+Route::get('/doctor/{doctor}', [DoctorController::class, 'show']);
