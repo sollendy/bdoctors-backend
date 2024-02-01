5ce3dcf2947ac84ff3d009dece3535bc82678123
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2024-02-01 16:49:30 +0100

    created api controller to save stars in database

--- app/Http/Controllers/Api/StarController.php
+++ app/Http/Controllers/Api/StarController.php
@@ -4,13 +4,26 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\User;
+use App\Models\Profile;
 use App\Models\Star;
 
 class StarController extends Controller
 {
     public function store(Request $request, $id)
     {
-        //
+        $doctor = Profile::findOrFail($id);
+        $star= Star::findOrFail($request->input('star_id'));
+
+
+        $doctor->stars()->attach($star, ['date' => date('Y-m-d')]);
+
+
+        return response()->json(
+            [
+                'success' => true,
+            ]
+        );
+
     }
 }
+
